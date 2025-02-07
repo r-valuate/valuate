05e90d72f929f6f53b6937ddf099a71e0b990441
carranzajuanp <carranzajuanp@gmail.com>
2025-02-07 14:35:19 -0300

    up

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,3 @@
-utils::globalVariables(c("na.omit", "Variable", "Pr(>|z|)", "impactos$total",
+utils::globalVariables(c("na.omit", "Variable", "Pr(>|z|)", "impactos$total","Freq",
                          "Estimate", "Valor", "mediana", "id", "tipo", "impacto",
                          "condicion", "qrf", "rowIndex", "mape","rowIndex", "mape", "quantile","qrf"))
